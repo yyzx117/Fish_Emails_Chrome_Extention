3615b76df4a8c78cbc1fe744b1acab9f41bda025
xguo686 <112277260+xguo686@users.noreply.github.com>
2022-10-22 17:05:23 -0400

    Add files via upload

--- extractURLs.js
+++ extractURLs.js
@@ -3,6 +3,6 @@ let urls = extractURLs(email);
 document.getElementById("urls").innerHTML = urls;
 
 function extractURLs(text) {
-    let urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/g;
+    let urlRegex = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/g;
     return text.match(urlRegex);
 }
