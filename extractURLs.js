15fe8433adef20a7a945c2c4ef35d000207a93a7
yyzx117 <97866407+yyzx117@users.noreply.github.com>
2022-10-23 23:56:33 -0400

    Delete extractURLs.js

--- extractURLs.js
+++
@@ -1,8 +0,0 @@
-let email = prompt("Enter your text: ");
-let urls = extractURLs(email);
-document.getElementById("urls").innerHTML = urls;
-
-function extractURLs(text) {
-    let urlRegex = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/g;
-    return text.match(urlRegex);
-}
